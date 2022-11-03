f46fbed1ee7289fbe98d397c7da700bd784a026d
Sandeep-Dora <82802512+Sandeep-Dora@users.noreply.github.com>
2022-11-03 22:01:45 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -12,6 +12,7 @@ def test_add_employee():
 def test_delete_inexistent_employee():
     response = client.delete("/employees",json={"name": "Non_Existing_Employee"})
     assert response.status_code == 200
+    assert response.json() == 'Non_Existing_Employee'               # Will throw the name of the "Non_Existing_Employee" in the response if name is not found
 
 
 def test_delete_employee():
@@ -22,6 +23,18 @@ def test_delete_employee():
 def test_add_employee_with_existing_id():
     response = client.post("/employees",json={"name": "Employee_Name","title": "Employee_Title","salary": 30000})
     assert response.status_code == 200
+    
+def test_add_employee_with_empty_data():
+    response = client.post("/employees",son={}    )
+    assert response.status_code == 400                              # Error reponse code for mandatory fields missing   
+
+    
+def test_add_multiple_employees(name, title, salary):
+    for i in range(0, 3):
+        response = client.post("/employees",json={"name": name,"salary": salary,"title": title}
+        assert response.status_code == 200
+        assert response.json()[name]['title'] == title
+        assert response.json()[name]['salary'] == salary        
 
 
 def test_get_all_employees():
